3722b51862bbd944bf1f9f2dc8f066f7d8ddf60c
Tyler Alterio <tyalt1@gmail.com>
2025-02-11 19:46:03 -0500

    upgrade to elixir 1.18

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Podcaster.MixProject do
     [
       app: :podcaster,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
